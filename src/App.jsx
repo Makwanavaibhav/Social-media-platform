11cad3c6adeffa04fb859db7366ded182c20864d
Makwanavaibhav <vaibhavmakwana617@gmail.com>
2026-02-13 15:18:03 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-amber-50 p-8">
+      
+    </div>
+  )
+}
+
+export default App;
